3c7950bf37d376a4cccfd57ac2fe58ab55231640
ishalawa <contact@halawa.dev>
2022-10-09 01:28:20 +0200

    Components Tree

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -27,24 +27,28 @@ function App() {
 			<label htmlFor="search">Search: </label>
 			<input id="search" type="text" />
 			<hr />
-			<ul>
-				{
-					list.map(item => {
-						return (
-							<li key={item.objectID}>
-								<span>
-									<a href={item.url}>{item.title}</a>
-								</span>
-								<span>{item.author}</span>
-								<span>{item.num_comments}</span>
-								<span>{item.points}</span>
-							</li>
-						)
-					})
-				}
-			</ul>
+			<List />
 		</div>
 	);
+}
+
+function List() {
+	return (
+		<ul>
+			{
+				list.map(item => (
+					<li key={item.objectID}>
+						<span>
+							<a href={item.url}>{item.title}</a>
+						</span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+					</li>
+				))
+			}
+		</ul>
+	)
 }
 
 export default App;
