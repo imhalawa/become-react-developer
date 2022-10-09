e38440984701c9ce8e49ce0dc0b964417e0cc2eb
ishalawa <contact@halawa.dev>
2022-10-09 23:01:09 +0200

    Advanced Component Definition (Arrow Function)

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,41 +1,46 @@
 import './App.css';
 
-class Developer {
-	constructor(firstName, lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+const list = [
+	{
+		title: 'React',
+		url: 'https://reactjs.org/',
+		author: 'Jordan Walke',
+		num_comments: 3,
+		points: 4,
+		objectID: 0,
+	},
+	{
+		title: 'Redux',
+		url: 'https://redux.js.org/',
+		author: 'Dan Abramov, Andrew Clark',
+		num_comments: 2,
+		points: 5,
+		objectID: 1,
+	},
+];
 
-	get fullName() {
-		return this.firstName + ' ' + this.lastName;
-	}
-}
-
-
-function App() {
+const App = () => {
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
-
 			<label htmlFor="search">Search: </label>
 			<input id="search" type="text" />
 			<hr />
-			<List />
 			<List />
 		</div>
 	);
 }
 
-function List() {
-	const robin = new Developer('Robin', 'Wieruch');
-	const dennis = new Developer('Dennis', 'Wieruch');
-
-	return (
-		<div>
-			{robin.fullName}
-			{dennis.fullName}
+const List = () =>
+	list.map(item => (
+		<div key={item.objectID}>
+			<span>
+				<a href={item.url}>{item.title}</a>
+			</span>
+			<span>{item.author}</span>
+			<span>{item.num_comments}</span>
+			<span>{item.points}</span>
 		</div>
-	)
-}
+	));
 
 export default App;
