f2702e65ca71af4041e76f2ebedc3bc2d0ffa6ca
ishalawa <contact@halawa.dev>
2022-10-09 23:10:14 +0200

    Handling Events in React

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -19,12 +19,21 @@ const list = [
 	},
 ];
 
+// Read more about synthetic events: https://reactjs.org/docs/events.html
+
 const App = () => {
+
+	const handleChange = event =>{
+		console.log(event.target.value);
+	}
+
+
+
 	return (
 		<div>
 			<h1>My Hacker Stories</h1>
 			<label htmlFor="search">Search: </label>
-			<input id="search" type="text" />
+			<input onChange={handleChange} id="search" type="text" />
 			<hr />
 			<List />
 		</div>
